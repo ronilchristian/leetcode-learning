3b110b737611320640c19756d826afbf4f01379c
ronilchristian <ronilrc13@gmail.com>
2023-12-10 17:42:30 -0600

    fast and slow 2

---
+++ phase1/fast-and-slow-pointers/2 - 203 - Remove Linked List Elements.java
@@ -0,0 +1,33 @@
+// 203 - Remove Linked List Elements
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        // defining pointers
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = head;
+
+        while (cur != null) {
+            if (cur.val == val) {
+                prev.next = cur.next;
+            }
+            else {
+                prev = cur;
+            }
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+}
