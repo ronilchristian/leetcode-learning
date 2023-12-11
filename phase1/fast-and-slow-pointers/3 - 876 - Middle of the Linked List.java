60f3888535963a46da46dd2cc43b8660aaae2b6f
ronilchristian <ronilrc13@gmail.com>
2023-12-11 15:19:29 -0600

    fast and slow 3

---
+++ phase1/fast-and-slow-pointers/3 - 876 - Middle of the Linked List.java
@@ -0,0 +1,27 @@
+// 876 - Middle of the Linked List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        // base case
+        if (head.next == null) return head;
+
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+}
