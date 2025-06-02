b8a87e4b8cce70aa6e601179090ef27ea2584f63
ronilchristian <ronilrc13@gmail.com>
2025-06-02 14:13:12 -0500

    add progress

---
+++ phase1/two-pointers/6_1 - 15 - 3Sum.java
@@ -0,0 +1,42 @@
+// 6_1 - 15 - 3Sum
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // base case
+        List<List<Integer>> res = new LinkedList<>();
+        int n = nums.length;
+        if (n < 3) return res;
+
+        // sort array
+        Arrays.sort(nums);
+
+        // iterate
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+
+            int start = i + 1, end = n - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum == 0) {
+                    List<Integer> sub = new LinkedList<>();
+                    sub.add(nums[i]);
+                    sub.add(nums[start]);
+                    sub.add(nums[end]);
+                    res.add(sub);
+
+                    start++;
+                    end--;
+
+                    while (start < end && nums[start] == nums[start - 1]) start++;
+                    while (start < end && nums[end] == nums[end + 1]) end--;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+
+        return res;
+    }
+}
